f692f08c351e8701083fe74286f2f745119b28a4
eclppppuvr <valeria.kirilovskih@yandex.com>
2025-10-01 14:40:01 +0500

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -13,10 +13,30 @@ function isPalindrome(string) {
 function extractNumber(input) {
   const string = String(input);
   const digits = string.replace(/\D/g, '');
-  
+
   if (digits.length === 0) {
     return NaN;
   }
-  
+
   return parseInt(digits, 10);
 }
+// Проверка длины строки
+checkStringLength('проверяемая строка', 20); // true
+checkStringLength('проверяемая строка', 18); // true
+checkStringLength('проверяемая строка', 10); // false
+
+// Проверка палиндрома
+isPalindrome('топот'); // true
+isPalindrome('Довод'); // true
+isPalindrome('Кекс'); // false
+isPalindrome('Лёша на полке клопа нашёл '); // true
+
+// Извлечение чисел
+extractNumber('2023 год'); // 2023
+extractNumber('ECMAScript 2022'); // 2022
+extractNumber('1 кефир, 0.5 батона'); // 105
+extractNumber('агент 007'); // 7
+extractNumber('a я томат'); // NaN
+extractNumber(2023); // 2023
+extractNumber(-1); // 1
+extractNumber(1.5); // 15
