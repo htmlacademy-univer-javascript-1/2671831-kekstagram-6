b11e5e037d41c2d10dcc9e57232249fddd8bd0db
Keks <keks@htmlacademy.ru>
2025-11-26 15:51:46 +0300

    :heavy_check_mark: Сборка #13

---
+++ 13/js/pictures.js
@@ -0,0 +1,37 @@
+import {openBigPicture} from './bigPicture.js';
+
+const pictures = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+
+const createPictureElement = (picture) => {
+  const { url, description, likes, comments } = picture;
+  const pictureElement = pictureTemplate.cloneNode(true);
+
+  pictureElement.querySelector('.picture__img').src = url;
+  pictureElement.querySelector('.picture__img').alt = description;
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
+  pictureElement.querySelector('.picture__likes').textContent = likes;
+
+  pictureElement.addEventListener('click', (evt) => {
+    evt.preventDefault();
+
+    openBigPicture(picture);
+  });
+
+  return pictureElement;
+};
+
+const renderPhotos = (photos) => {
+  const picturesFragment = document.createDocumentFragment();
+
+  photos.forEach((photo) => {
+    picturesFragment.appendChild(createPictureElement(photo));
+  });
+
+  pictures.appendChild(picturesFragment);
+};
+
+export {renderPhotos};
